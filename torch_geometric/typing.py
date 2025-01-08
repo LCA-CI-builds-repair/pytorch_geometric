effa82976bdba7403139a0cab32c777b44bba5aa
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-08 12:24:12 +0000

    waleko__gpt-4o_fa5a__id_0

--- torch_geometric/typing.py
+++ torch_geometric/typing.py
@@ -42,7 +42,7 @@ try:
     WITH_SAMPLED_OP = hasattr(pyg_lib.ops, 'sampled_add')
     WITH_INDEX_SORT = hasattr(pyg_lib.ops, 'index_sort')
     WITH_METIS = hasattr(pyg_lib, 'partition')
-    WITH_EDGE_TIME_NEIGHBOR_SAMPLE = ('edge_time' in inspect.signature(
+    with_edge_time_neighbor_sample = ('edge_time' in inspect.signature(
         pyg_lib.sampler.neighbor_sample).parameters)
     WITH_WEIGHTED_NEIGHBOR_SAMPLE = ('edge_weight' in inspect.signature(
         pyg_lib.sampler.neighbor_sample).parameters)
